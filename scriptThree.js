1d9eed515bb0e7f937203b640ea2f6a2a97c7769
vivianvnl <224661527+vivianvnl@users.noreply.github.com>
2025-09-05 16:02:00 -0500

    add js event listeners

--- scriptThree.js
+++ scriptThree.js
@@ -4,10 +4,12 @@ let playerTwo = 'o';
 const squares = document.querySelectorAll('.squares');
 
 squares.forEach(square => {
-    const x = document.createElement('p');
-    const o = document.createElement('p');
-    x.textContent = "x";
-    o.textContent = "o";
-    //square.appendChild(x);
-    square.appendChild(o);
+    square.addEventListener('click', function(event) {    
+        const x = document.createElement('p');
+        const o = document.createElement('p');
+        x.textContent = "x";
+        o.textContent = "o";
+        square.appendChild(x);
+        //square.appendChild(o);
+    });
 });
