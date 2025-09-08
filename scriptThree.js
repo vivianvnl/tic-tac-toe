44385931765ac70365ac212e10c507941c5f13bc
vivianvnl <224661527+vivianvnl@users.noreply.github.com>
2025-09-08 16:05:47 -0500

    add checkdraw function, no double clicking spaces

--- scriptThree.js
+++ scriptThree.js
@@ -1,53 +1,68 @@
 let grid = [];
 
 const playGame = (function() {
-    let playerOne = 'x';
-    let playerTwo = 'o';
+    const players = {
+        playerOne: 'x',
+        playerTwo: 'o'
+    };
 
-    let currentPlayer = playerOne;
+    let currentPlayer = players.playerOne;
 
     function switchPlayer() {
-        currentPlayer = (currentPlayer == playerOne) ? playerTwo : playerOne;
+        currentPlayer = (currentPlayer == players.playerOne) ? players.playerTwo : players.playerOne;
     }
 
     const squares = document.querySelectorAll('.squares');
+    
     squares.forEach(square => {
         grid.push(square);
+        
         square.addEventListener('click', function(event) {    
-            const x = document.createElement('p');
-            const o = document.createElement('p');
-            x.textContent = "x";
-            o.textContent = "o";
-
-            if (currentPlayer == playerOne) {
-                square.appendChild(x);
-                switchPlayer();
-                checkWin();
+            const playerMove = document.createElement('p');
+            playerMove.textContent = currentPlayer;
+            
+            if (event.currentTarget.textContent) {
+                alert ('This spot has already been chosen. Try again!');
             }
-
-            else if (currentPlayer == playerTwo) {
-                square.appendChild(o);
-                switchPlayer();
+            else {
+                square.appendChild(playerMove);
                 checkWin();
+                if (checkWin !== 'win') {
+                    checkDraw();
+                    switchPlayer();
+                }
+                
             }
-
+            
+            function checkDraw() {
+                for (let i = 0; i < grid.length; i++) {
+                    if (!grid[i].textContent) {
+                        return;
+                    }
+                    else {
+                        continue;
+                    }
+                }
+                console.log('Draw!');
+            }
+            
             function checkWin() {
                 const gridWinners = [
                 [0,1,2], [3,4,5], [6,7,8],
                 [0,3,6], [1,4,7], [2,5,8],
                 [0,4,8], [2,4,6]
                 ]
-                console.log(event.currentTarget.textContent);
+
                 for (const combo of gridWinners) {
                     const [a, b, c] = combo;
                     if (
-                        !grid[a].textContent == "" &&
+                        grid[a].textContent &&
                         grid[a].textContent === grid[b].textContent &&
                         grid[a].textContent === grid[c].textContent
                     ) {
-                        switchPlayer();
-                        console.log(`${currentPlayer} wins!`)
-                        return;
+                        console.log(`${currentPlayer} wins!`);
+                        return 'win';
+                        
                     } else {
                         continue;
                     }
@@ -56,4 +71,3 @@ const playGame = (function() {
         });
     });  
 })();
-console.log(grid);
