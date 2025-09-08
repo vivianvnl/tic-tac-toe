54aa88f5f06076e9142b9522851d345defbdf5aa
vivianvnl <224661527+vivianvnl@users.noreply.github.com>
2025-09-08 15:02:20 -0500

    add working checkwin function

--- scriptThree.js
+++ scriptThree.js
@@ -21,45 +21,39 @@ const playGame = (function() {
 
             if (currentPlayer == playerOne) {
                 square.appendChild(x);
-                //switchPlayer();
+                switchPlayer();
                 checkWin();
             }
 
             else if (currentPlayer == playerTwo) {
                 square.appendChild(o);
-                //switchPlayer();
+                switchPlayer();
                 checkWin();
             }
 
             function checkWin() {
                 const gridWinners = [
                 [0,1,2], [3,4,5], [6,7,8],
-                [1,3,6], [1,4,7], [2,5,8],
-                [1,4,8], [2,4,7]
+                [0,3,6], [1,4,7], [2,5,8],
+                [0,4,8], [2,4,6]
                 ]
-
-                for (let i = 0; i < gridWinners.length; i++) {
-                    for (let j = 0; j < 3; j++) {
-                        //console.log(grid[[i][j]].textContent);
-                        if (grid[[i][j]].textContent !== 'x' || grid[[i][j]].textContent === undefined) {
-                            return;
-                        }
-                        if (j === 2 && grid[[i][j]].textContent === 
-                            grid[[i][j-1]].textContent ===
-                             grid[[i][j-2]].textContent) {
-                                console.log('Player 1 wins!');
-                        }
-                        if (grid[[i][j]].textContent === 'x') {
-                            continue;
-                        }
+                console.log(event.currentTarget.textContent);
+                for (const combo of gridWinners) {
+                    const [a, b, c] = combo;
+                    if (
+                        !grid[a].textContent == "" &&
+                        grid[a].textContent === grid[b].textContent &&
+                        grid[a].textContent === grid[c].textContent
+                    ) {
+                        switchPlayer();
+                        console.log(`${currentPlayer} wins!`)
+                        return;
+                    } else {
+                        continue;
                     }
-                    return;
                 }
             }
-        console.log(grid[0].textContent)
-        console.log(event.currentTarget.textContent);
         });
-    });
-    console.log(grid)
-    
+    });  
 })();
+console.log(grid);
